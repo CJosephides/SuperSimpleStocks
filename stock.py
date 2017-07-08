abf73795eeb9d001cccc1f652546507cfdb24f31
Christos Josephides <cjosephides@gmail.com>
2017-07-08 15:00:39 +0100

    Linted code.

--- stock.py
+++ stock.py
@@ -11,8 +11,8 @@ from math import log
 from math import exp
 import datetime
 
-# Named tuple represents stock trades. 
-Trade = namedtuple('Trade', ['buy_sell', 'quantity', 'trade_price', 
+# Named tuple represents stock trades.
+Trade = namedtuple('Trade', ['buy_sell', 'quantity', 'trade_price',
                              'trade_time'])
 """
 buy_sell: +1 for buying; -1 for selling.
@@ -25,17 +25,17 @@ trade_time: a datetime time stamp for the trade record.
 class Stock:
     """Super simple stock.
 
-    Trades relevant to this stock are stored in a `semi-private' list 
+    Trades relevant to this stock are stored in a `semi-private' list
     attribute, _trades, each as namedtuple object.
-    
+
     Attributes
     ----------
-    symbol: alphabetic string indicating the shorthand name of the stock. 
+    symbol: alphabetic string indicating the shorthand name of the stock.
             Symbols uniquely identify each stock in a static dictionary.
     stype: a string in {'common', 'preferred'} indicating stock's type.
-    last_dividend: a non-negative integer (in pennies) of the stock's last 
+    last_dividend: a non-negative integer (in pennies) of the stock's last
                    dividend.
-    fixed_dividend: a percentile in [0., 1.], or nan, of the stock's fixed 
+    fixed_dividend: a percentile in [0., 1.], or nan, of the stock's fixed
                     dividend.
     par_value: a non-negative integer (in pennies) of the stock's par value.
     """
@@ -43,29 +43,29 @@ class Stock:
     # Static variables for the Stock class
     stocks = {}  # a dict to help with stock-wide calculations
 
-    def __init__(self, symbol, stype, last_dividend, fixed_dividend, 
+    def __init__(self, symbol, stype, last_dividend, fixed_dividend,
                  par_value):
         """
-        Initializes a stock. 
+        Initializes a stock.
 
-        The (static) Stock.stocks dictionary is automatically updated with the 
-        new entry. The stock's symbol must be unique; if the symbol already 
-        exists, then the previous class instance with this symbol will be 
+        The (static) Stock.stocks dictionary is automatically updated with the
+        new entry. The stock's symbol must be unique; if the symbol already
+        exists, then the previous class instance with this symbol will be
         overwritten.
         """
-        
+
         # Do a minimum amount of validation.
         assert symbol.isalpha(),\
             'stock symbol, %r,  is not alphabetic' % symbol
         assert stype.lower() in ('common', 'preferred'),\
             'stock type, %r, must be "common" or "preferred"' % stype
         assert (isinstance(last_dividend, int) and last_dividend >= 0),\
-            ('stock last_dividend, %r, must be a non-negative integer' 
+            ('stock last_dividend, %r, must be a non-negative integer'
              % last_dividend)
         assert (fixed_dividend is nan) or \
-            (isinstance(fixed_dividend, float) and 
+            (isinstance(fixed_dividend, float) and
              0. <= fixed_dividend <= 1.),\
-            ('stock fixed dividend, %r, must be a valid percentile in ' 
+            ('stock fixed dividend, %r, must be a valid percentile in '
              '[0., 1.] or nan' % fixed_dividend)
         assert (isinstance(par_value, int) and par_value >= 0),\
             'stock par_value, %r, must be a non-negative integer' % par_value
@@ -76,12 +76,12 @@ class Stock:
         self.fixed_dividend = fixed_dividend
         self.par_value = par_value
 
-        # Initialize a list of trades for this stock. We want this to be 
+        # Initialize a list of trades for this stock. We want this to be
         # semi-private and accessible by some interface instead, so we'll
         # underscore it.
         self._trades = []
 
-        # Add to the class dictionary. The stocks are uniquely identified by 
+        # Add to the class dictionary. The stocks are uniquely identified by
         # their symbol.
         Stock.stocks[symbol] = self  # a reference to the stock instance
 
@@ -96,30 +96,26 @@ class Stock:
         Print a simple summary of the stock.
         """
         return ('{0:s} ({1:s}): LastDiv: {2:d}, FixedDiv: {3:.2f}, '
-                'ParVal: {4:d}'.format(self.symbol, self.stype, 
+                'ParVal: {4:d}'.format(self.symbol, self.stype,
                                        self.last_dividend, self.fixed_dividend,
                                        self.par_value))
 
     def dividend_yield(self):
         """
         Return the stock's dividend yield.
-        
+
         The calculation depends on the type of the stock.
         The ticker price is calculated from the stock's trading in the past 15
         minutes; if there have been no trades use the par value of the stock.
         """
 
-        #if len(self._trades) > 0:
-        #    tp = self.stock_price()  # default time window is 15 minutes
-        #else:
-        #    tp = self.par_value
-        tp = self.stock_price()
+        ticker_price = self.stock_price()
 
         # Force float division (standard in Python 3.x)
         if self.stype == 'common':
-            return float(self.last_dividend) / tp
-        else:
-            return float(self.fixed_dividend) * self.par_value / tp
+            return float(self.last_dividend) / ticker_price
+
+        return float(self.fixed_dividend) * self.par_value / ticker_price
 
     def price_earnings_ratio(self):
         """
@@ -129,15 +125,11 @@ class Stock:
         minutes; if there have been no trades use the par value of the stock.
         """
 
-        #if len(self._trades) > 0:
-        #    tp = self.stock_price()  # default time window is 15 minutes
-        #else:
-        #    tp = self.par_value
-        tp = self.stock_price()
+        ticker_price = self.stock_price()
 
         # Assume 'dividend' means 'last dividend' in the specification.
         # NOTE I am not familiar with these terms, so I may be wrong.
-        return tp / self.last_dividend  # force float division
+        return ticker_price / self.last_dividend  # force float division
 
     def buy(self, quantity, trade_price, trade_time=None):
         """
@@ -175,7 +167,7 @@ class Stock:
 
     def _record_trade(self, buy_sell, quantity, trade_price, trade_time):
         """
-        Records a buy or sell trade in the trade list. 
+        Records a buy or sell trade in the trade list.
 
         The Stock.buy and Stock.sell methods invoke this method.
         Trades are stored as a named tuple (one per trade).
@@ -200,30 +192,32 @@ class Stock:
                       defaults to 15 minutes (before now).
         """
 
-        # If there have been no stock trades at all, then return the par value 
+        # If there have been no stock trades at all, then return the par value
         # of the stock.
-        if len(self._trades) == 0:
+        if self._trades is False:
             return self.par_value
-        
+
         # If we have any trades, we can attempt to calculate a mean.
         trade_value = 0
         trade_quantity = 0
-        # Generate an iterable by selecting trades within the time window; 
+        # Generate an iterable by selecting trades within the time window;
         # then, do a simple weighted mean calculation.
-        for t in filter(lambda t: (datetime.datetime.now() - time_window <= 
-                        t.trade_time <= datetime.datetime.now()),
-                        self._trades):
-            trade_value += t.trade_price * t.quantity
-            trade_quantity += t.quantity
+        for trade in filter(lambda trade: (datetime.datetime.now() -
+                                           time_window <=
+                                           trade.trade_time <=
+                                           datetime.datetime.now()),
+                            self._trades):
+            trade_value += trade.trade_price * trade.quantity
+            trade_quantity += trade.quantity
 
         if trade_quantity > 0:
             return float(trade_value) / trade_quantity  # force float division
-        else:
-            # If all trades happened before the specified time window, return
-            # the par value.
-            # NOTE This is not ideal. Perhaps a better implementation 
-            # would be to raise an exception and request a longer time window.
-            return self.par_value
+
+        # If all trades happened before the specified time window, return
+        # the par value.
+        # NOTE This is not ideal. Perhaps a better implementation
+        # would be to raise an exception and request a longer time window.
+        return self.par_value
 
     @staticmethod
     def gbce_all_share_index():
@@ -234,16 +228,16 @@ class Stock:
         """
 
         # Do the calculation in log space to avoid working with scary
-        # exponents. If the ticker price is 0, we'll exclude that stock from 
+        # exponents. If the ticker price is 0, we'll exclude that stock from
         # the geometric mean.
         log_sum = 0
         n_zeros = 0
-        for symbol, stock in Stock.stocks.items():
-            tp = stock.stock_price()
-            if tp == 0:
+        for _, stock in Stock.stocks.items():
+            ticker_price = stock.stock_price()
+            if ticker_price == 0:
                 n_zeros += 1
                 continue
-            log_sum += log(tp)
+            log_sum += log(ticker_price)
 
         # Cover the edge case of all zeros.
         if n_zeros == len(Stock.stocks):
@@ -251,4 +245,3 @@ class Stock:
 
         # Exponentiate the answer before returning.
         return exp((1. / (len(Stock.stocks)  - n_zeros)) * log_sum)
-
